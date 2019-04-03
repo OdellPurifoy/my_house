d94acf56410bede3a0dae15ea4cca903af827e99
Odell <odell.purifoy@gmail.com>
2019-04-03 13:33:43 -0400

    model relationships and dtl controller updates

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
 
-  has_many :daily_task_lists
+  has_and_belongs_to_many :daily_task_list, optional: true
   has_many :users
 end
